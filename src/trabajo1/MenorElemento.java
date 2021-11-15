48ecbe3ba202d5bad9356f3df92d73ad73862dd6
Condori-William <wcondorian@unsa.edu.pe>
2021-11-14 19:12:46 -0500

    dscdcsdvc

--- src/trabajo1/MenorElemento.java
+++ src/trabajo1/MenorElemento.java
@@ -20,11 +20,20 @@ public class MenorElemento {
 	    
 		int [] arreglo = new int[cantidad];
 		
+		System.out.println("Llenar el Array");
+		for(int i = 0; i < arreglo.length ; i++ ){
+			arreglo[i] = sc.nextInt();
+		}
 		
+		System.out.println("El numero maximo es: "+maxRec(arreglo,0,arreglo.length-1));
 		System.out.println("El numero minimo es: "+minRec(arreglo,0,arreglo.length-1));
 	}
 
-	
+	private static int maxRec (int A [],int low, int high) {
+		if (low == high) return A [low];
+		int mid = (low+high)/2;
+		return Math.max(maxRec(A,low,mid), maxRec(A,mid+1,high));
+	}
 	private static int minRec (int A [],int low, int high) {
 		if (low == high) return A [low];
 		int mid = (low+high)/2;
